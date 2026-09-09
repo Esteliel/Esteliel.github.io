5b0cf9cae4cf24446881ed313edb28535dff87bf
nudoo <nudoo@qq.com>
2026-09-09 16:39:50 +0800

    拆分提示词管理独立站点

--- assets/js/prompts-config.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  'use strict';
-
-  window.ESTELIEL_SUPABASE = {
-    url: 'https://walunxpspenzbemskusm.supabase.co',
-    publishableKey: 'sb_publishable_8n_H9XxhqfzAJkUd0y9G9w_51-c1Dcy'
-  };
-})();
